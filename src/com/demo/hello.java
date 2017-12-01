84dfd6571e549e00b067f9f78ed9afe4c5b644dc
kdpatoliya <krunalpatoliya17@gmail.com>
2017-12-01 03:52:22 -0800

    First commit

---
+++ src/com/demo/hello.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Saying Hey");
+		System.out.println("Saying Hi");
+		System.out.println("Saying Hey");
+		System.out.println("Saying Hi");
+
+	}
+
+}
